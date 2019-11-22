639caf57082d4b3b3b25e7bfee001fe2a2f8c7b3
JiachenYu <yu.jiac@husky.neu.edu>
2019-11-22 16:15:11 -0500

    Update routing table and add link to each page to navbar

--- Client-Angular/src/app/app.module.ts
+++ Client-Angular/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { PostComponent } from './rides/post/post.component';
+import { SearchComponent } from './rides/search/search.component';
+import { LoginComponent } from './users/login/login.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    PostComponent,
+    SearchComponent,
+    LoginComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
