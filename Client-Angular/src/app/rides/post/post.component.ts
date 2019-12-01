ac2ca6d36b0c5509e25a8f84ce5355b247e9a3da
rajopadhyev <rajopadhye.v@husky.neu.edu>
2019-11-30 23:38:13 -0500

    Modified post html page

--- Client-Angular/src/app/rides/post/post.component.ts
+++ Client-Angular/src/app/rides/post/post.component.ts
@@ -6,10 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./post.component.scss']
 })
 export class PostComponent implements OnInit {
-
+  currentDate: string;
+  hours: string;
   constructor() { }
 
   ngOnInit() {
+
+    this.currentDate = new Date().toISOString().split('T')[0];
+      let date = new Date();
+      this.hours = date.getHours().toLocaleString();
+      const element = document.getElementById('date') as HTMLInputElement;
+      element.valueAsNumber =
+        Date.now() - new Date().getTimezoneOffset() * 60000;
   }
 
 }
