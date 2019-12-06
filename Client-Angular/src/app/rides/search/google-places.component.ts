d17c9b5247f6f493d14bb64c1ca5661475f3d4b2
modipriyam <55067505+modipriyam@users.noreply.github.com>
2019-12-05 22:28:03 -0500

    Adding onInIt components and search Restrictions

--- Client-Angular/src/app/rides/search/google-places.component.ts
+++ Client-Angular/src/app/rides/search/google-places.component.ts
@@ -12,3 +12,39 @@ import { } from '@types/googlemaps';
         >
     `,
 })
+export class AutocompleteComponent implements OnInit, AfterViewInit {
+    @Input() adressType: string;
+    @Output() setAddress: EventEmitter<any> = new EventEmitter();
+    @ViewChild('addresstext') addresstext: any;
+
+    autocompleteInput: string;
+    queryWait: boolean;
+
+    constructor() {
+    }
+
+    ngOnInit() {
+    }
+
+    ngAfterViewInit() {
+        this.getPlaceAutocomplete();
+    }
+
+    private getPlaceAutocomplete() {
+        const autocomplete = new google.maps.places.Autocomplete(this.addresstext.nativeElement,
+            {
+                componentRestrictions: { country: 'US' },
+                types: [this.adressType]  // 'establishment' / 'address' / 'geocode'
+            });
+        google.maps.event.addListener(autocomplete, 'place_changed', () => {
+            const place = autocomplete.getPlace();
+            this.invokeEvent(place);
+        });
+    }
+
+    invokeEvent(place: Object) {
+        this.setAddress.emit(place);
+        console.log();
+    }
+
+}
