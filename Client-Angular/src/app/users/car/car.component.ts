8834517074ece3b7f6d956d520b53a8451f5f786
JiachenYu <yu.jiac@husky.neu.edu>
2019-12-08 19:13:28 -0500

    Set up client for add and get car information

--- Client-Angular/src/app/users/car/car.component.ts
+++ Client-Angular/src/app/users/car/car.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../../services/user.service';
+import { CarService } from '../../services/car.service';
+import { User } from '../../models/user.model';
+import { Car } from '../../models/car.model';
+import { Router, ActivatedRoute } from '@angular/router';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-car',
@@ -6,10 +12,51 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./car.component.scss']
 })
 export class CarComponent implements OnInit {
+  currentUser: User;
+  userCar: Car;
+  carForm: FormGroup;
+  colors = ['BLACK', 'WHITE', 'GRAY', 'SILVER', 'BLUE', 'RED', 'BROWN', 'GOLD', 'GREEN', 'TAN', 'ORANGE'];
 
-  constructor() { }
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private userService: UserService,
+    private router: Router,
+    private route: ActivatedRoute,
+    private carService: CarService
+  ) { }
 
   ngOnInit() {
+    this.carForm = this.formBuilder.group({
+      brand: ['', Validators.required],
+      model: ['', Validators.required],
+      color: ['', Validators.required],
+      plate: ['', [Validators.required, Validators.pattern('[A-Z0-9]{5,7}')]]
+    });
+
+    if((this.currentUser = this.userService.currentUserValue)){
+
+    }
+    else{
+      this.router.navigate(['/login'], {queryParams: {returnUrl: this.router.url}});
+    }
+    
+  }
+
+  get formControls(){
+    return this.carForm.controls;
+  }
+
+  onSubmit(){
+    this.carService.add(this.carForm.value, this.currentUser._id)
+      .subscribe(
+        data => {
+          this.router.navigate(['/personal_home']);
+        },
+        error => {
+          window.alert(error.message);
+        }
+      )
   }
 
 }
--- Client-Angular/src/app/users/personal-home/personal-home.component.ts
+++ Client-Angular/src/app/users/personal-home/personal-home.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from '../../services/user.service';
+import { CarService } from '../../services/car.service';
 import { User } from '../../models/user.model';
 import { Router, ActivatedRoute } from '@angular/router';
-import { Statement } from '@angular/compiler';
+import { Car } from 'src/app/models/car.model';
 
 @Component({
   selector: 'app-personal-home',
@@ -11,16 +12,22 @@ import { Statement } from '@angular/compiler';
 })
 export class PersonalHomeComponent implements OnInit {
   currentUser: User;
+  currentCar: Car;
 
   constructor(
     private userService: UserService,
+    private carService: CarService,
     private router: Router,
     private route: ActivatedRoute,
   ) { }
 
   ngOnInit() {
     if((this.currentUser = this.userService.currentUserValue)){
-      //Check car and ride information
+      console.log("sending car request");
+      this.carService.get(this.currentUser._id)
+        .subscribe((car)=>{
+          this.currentCar = car;
+        })
     }
     else{
       this.router.navigate(['/login'], {queryParams: {returnUrl: this.router.url}});
