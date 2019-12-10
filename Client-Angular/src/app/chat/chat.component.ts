3dd85d8c64c2aaa9ae11751580b13dbfe0409385
harshitmandada1 <55108110+harshitmandada1@users.noreply.github.com>
2019-12-10 00:24:53 -0500

    updating the chat page with rooms and usernames

--- Client-Angular/src/app/chat/chat.component.ts
+++ Client-Angular/src/app/chat/chat.component.ts
@@ -9,28 +9,53 @@ import { ChatService } from '../services/chat.service';
   styleUrls: ['./chat.component.scss']
 })
 export class ChatComponent implements OnInit {
- message: String;
- messages: string[] = [];
+ user:String;
+ room:String;
+ messageText:String;
+ messageArray:Array<{user:String,message:String}> = [];
+ constructor(private _chatService:ChatService){
+     this._chatService.newUserJoined()
+     .subscribe(data=> this.messageArray.push(data));
 
-  constructor(private chatservice: ChatService) { }
 
+     this._chatService.userLeftRoom()
+     .subscribe(data=>this.messageArray.push(data));
 
+     this._chatService.newMessageReceived()
+     .subscribe(data=>this.messageArray.push(data));
+ }
 
   ngOnInit() {
-    this.chatservice
-      .getMessages()
-      .subscribe((message: string) => {
-        this.messages.push(message);
-      });
+    // this.chatservice
+    //   .getMessages()
+    //   .subscribe((message: string) => {
+    //     this.messages.push(message);
+    //   });
   }
 
 
+
+  join(){
+    this._chatService.joinRoom({user:this.user, room:this.room});
+}
+
+leave(){
+    this._chatService.leaveRoom({user:this.user, room:this.room});
+}
+
+sendMessage()
+{
+    this._chatService.sendMessage({user:this.user, room:this.room, message:this.messageText});
+}
+
+
+
+  
+
+    
   
   
 
-  sendMessage(){
-    this.chatservice.sendMessage(this.message);
-    this.message = '';
-  }
+
 
 }
